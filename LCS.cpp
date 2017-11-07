edaef333c2f08515d0b96db6c8cf8efbb487e52f
Md. Zahid Hasan <hasan84848484@gmail.com>
2017-11-07 14:14:38 +0600

    Update LCS.cpp

--- LCS.cpp
+++ LCS.cpp
@@ -2,51 +2,47 @@
 
 using namespace std;
 
-int c[100][100],i,j,m,n;
-char b[100][100],x[100],y[100];
+int i, j, m, n, c[20][20], k = 0;
+char x[20], y[20], b[20][20];
+char ans[20];
 
-void Print(int i, int j)
+char maxC(char a, char b)
 {
-    if(i == 0 || j == 0)return;
-    if(b[i][j] == 'c')
+    return ((a > b) ? a : b);
+}
+
+void print(int i,int j)
+{
+    if(i==0 || j==0)return;
+    if(b[i][j]=='c')
     {
-        Print(i-1, j-1);
+        print(i-1,j-1);
         printf("%c",x[i-1]);
+        ans[k++] = x[i-1];
     }
-    else if(b[i][j] == 'u')
-    {
-        Print(i-1, j);
-    }
-    else
-    {
-        Print(i, j-1);
-    }
+    else if(b[i][j]=='u')print(i-1,j);
+    else print(i,j-1);
 }
 
 void lcs()
 {
-    m = strlen(x);
-    n = strlen(y);
-    for(i=0; i<=m; i++)c[i][0] = 0;
-    for(i=0; i<=n; i++)c[0][i] = 0;
+    m=strlen(x);
+    n=strlen(y);
+    for(i=0; i<=m; i++)c[i][0]=0;
+    for(i=0; i<=n; i++)c[0][i]=0;
     for(i=1; i<=m; i++)
     {
         for(j=1; j<=n; j++)
         {
-            if(x[i-1] == y[j-1])
+            if(x[i-1]==y[j-1])
             {
-                c[i][j] = c[i-1][j-1];
-                b[i][j] = 'c';
-            }
-            else if(c[i-1][j] >= c[i][j-1])
-            {
-                c[i][j] = c[i-1][j];
-                b[i][j] = 'u';
+                c[i][j]=c[i-1][j-1]+1;
+                b[i][j]='c';
             }
             else
             {
-                c[i][j] = c[i][j-1];
-                b[i][j] = 'l';
+                c[i][j] = maxC(c[i-1][j], c[i][j-1]);
+                c[i-1][j] >= c[i][j-1] ? b[i][j]='u' : b[i][j]='l';
             }
         }
     }
@@ -54,13 +50,16 @@ void lcs()
 
 int main()
 {
-    memset(c, 0, sizeof(c));
-    memset(b, '\0', sizeof(b));
-    memset(x, '\0', sizeof(x));
-    memset(y, '\0', sizeof(y));
-    scanf("%s %s",x,y);
+    printf("Enter 1st sequence: ");
+    scanf("%s",x);
+    printf("Enter 2nd sequence: ");
+    scanf("%s",y);
+    printf("\nThe Longest Common Subsequence is: ");
     lcs();
-    Print(m,n);
-    puts("");
+    print(m,n);
+    printf("\n");
+    ans[k] = '\0';
+    int len = strlen(y);
+    printf("Matching Percentage: %.2lf%%\n",((k*1.00) / (len * 1.00)) * 100.00);
     return 0;
 }
